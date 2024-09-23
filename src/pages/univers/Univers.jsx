ede852ec0ef9221e09cda5a441db396120b7b4aa
“Hadicha” <hadichahalimjonova21@gmail.com>
2024-09-23 11:44:24 +0300

    universess

--- src/pages/univers/Univers.jsx
+++ src/pages/univers/Univers.jsx
@@ -13,12 +13,12 @@ function Univers({datas}) {
     <div className='universes'>
       <div className="container">
         <div className="univer_info">
-          <h2>{product[0].name}</h2>
+          {/* <h2>{product[0].name}</h2> */}
           <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio, velit! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde aliquam totam id quisquam tempore earum eius illum nulla neque distinctio quod magni odio, esse voluptatum deserunt itaque. Cupiditate, fuga dolorum!
           </p>
         </div>
         <div className="univer_img">
-          <img src={product[0].attachment.fileName} alt="" />
+          {/* <img src={product[0].attachment.fileName} alt="" /> */}
         </div>
       
       </div>
